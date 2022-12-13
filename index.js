d7d3662a63ed05fd4b4145ab0c4c61e7bf7168cd
caleigh <caleighsteill@outlook.com>
2022-12-12 17:28:09 -0700

    commit tree 5f010d62de0a35936ec15d2c4fedddfd1804c50a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2;
 
-// Write your code here
+const random = Math.ceil(Math.random())
+
+const num3 = 54
+const num4 = 10
+const mod = num3 % num4
+
+const max = Math.max(2, 4, 10, 13, 15, 20)
